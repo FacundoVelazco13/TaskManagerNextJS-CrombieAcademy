dd915d49570f02aad407985f76ca5db0750a232c
FacundoVelazco <facundovelazco133@gmail.com>
2024-12-16 19:36:35 -0300

    haciendo taskcards

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -2,9 +2,23 @@ import { Task } from '@/types/task'; // Adjust the import path as necessary
 
 export default async function TaskCard(task: { task: Task }) {
     return (
-      <div className="border-solid border-2 border-gray-200 p-4">
-        <h1>Task Card</h1>s
-        <p>{task.task.id}</p>
+      <div className="border-solid border-2 border-gray-200 p-4 rounded-lg">
+        <div className='flex'>
+            <h1>{task.task.title}</h1>
+        </div>
+        <div>
+            <h2>Descripcion : </h2>
+            <p>{task.task.description}</p>
+        </div>
+        <div>
+          <button className='p-2 m-2 bg-blue-500 text-white rounded-lg'>
+            edit
+          </button>
+          <button className='p-2 m-2 bg-red-500 text-white rounded-lg'>
+            delete
+          </button>
+        </div>
+
       </div>
     );
 }
--- src/components/TaskViewer.tsx
+++ src/components/TaskViewer.tsx
@@ -7,8 +7,9 @@ const prisma = new PrismaClient();
 export default async function TaskViewer() {
   const tasks : Task[] = await prisma.task.findMany();
   return (
-    <div className="border-solid border-2 border-gray-200 p-4">
-      <h1>Task List</h1>
+    <div className="border-solid border-2 border-gray-200 p-4 rounded-lg">
+      <h1 className="text-2xl">Task List</h1>
+      <hr className="my-2"/>
       <ul>
         {tasks.map((task : Task) => (
           <li key={task.id}>
